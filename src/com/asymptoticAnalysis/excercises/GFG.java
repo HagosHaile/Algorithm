e917c5485e543a395c3da94bb4a1e541791d82ee
HagosHaile <hagosb.haile@gmail.com>
2018-01-30 00:16:44 -0500

    Pascal's Triangle java code

--- src/com/asymptoticAnalysis/excercises/GFG.java
+++ src/com/asymptoticAnalysis/excercises/GFG.java
@@ -14,6 +14,25 @@ public class GFG {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int n = 5;
+		int[][] multi = new int[n][n];
+		multi[0][0]=1;
+		int j;
+		for (int i = 0; i < n; i++) {
+
+			for (j = 0; j <= i; j++) {
+				if (j == 0 || j == i) {
+					multi[i][j]=1;
+					System.out.print(multi[i][j]);
+				}
+				else {
+					multi[i][j]=multi[i-1][j-1] + multi[i-1][j];
+					System.out.print(multi[i][j]);
+
+				}
+			}
+			System.out.println();
+		}
 
 	}
 
