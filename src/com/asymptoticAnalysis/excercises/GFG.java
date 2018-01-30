12c9762219be9e6cc93049dfb4ffaf6ccb6b06f6
HagosHaile <hagosb.haile@gmail.com>
2018-01-29 22:28:34 -0500

    new solution

---
+++ src/com/asymptoticAnalysis/excercises/GFG.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.asymptoticAnalysis.excercises;
+
+/**
+ * @author Hagos B. Haile
+ *
+ */
+public class GFG {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
